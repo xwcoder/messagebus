235b9a892582c38e4ed4dc5b2ceb79a7f6f9f3db
xwcoder <wangwei111516@gmail.com>
2012-05-25 17:08:22 +0800

    MessageBus v1

---
+++ MessageBus-v2.js
@@ -0,0 +1,184 @@
+(function(window,undefined){
+    var id = 1;
+    var subTree = {t:{},h:[]};
+    var pubItems = {};
+    var config = {
+        cache : true
+    };
+    var toString = Object.prototype.toString;
+
+    var generateId = function(prefix){
+        prefix = prefix || '';
+        return id++;
+    };
+
+    var throwException = function(msg){
+        throw new Error(msg);
+    };
+
+    var illegalTopic = function(topic){
+        throwException('illegalTopic:' + topic);
+    };
+
+    var checkSubTopic = function(topic){
+        (!topic || !topic.length || toString.call(topic) != '[object String]' 
+            || (topic.indexOf('**') != -1 && !(/\*\*$/.test(topic)))
+            || (/\*{3}/.test(topic)) || /\.{2}/.test(topic)
+            || topic[0] == '.' || topic[topic.length-1] == '.') && illegalTopic(topic);
+    };
+
+    var checkIllegalCharactor = function(topic){
+        var m = /([\^])/.exec(topic);
+        if(m){
+            throwException('illegalCharactor:' + m[1]);
+        }
+    };
+
+    var checkPubTopic = function(topic){
+        (!topic || !topic.length || toString.call(topic) != '[object String]' 
+            || topic.indexOf('*') != -1 || topic[0] == '.' 
+            || /\.{2}/.test(topic)
+            || topic[topic.length] == '.') && illegalTopic(topic);
+    };
+
+    var doCall = function(topic, msg, handlers){
+        msg = msg || null;
+        var wrapFn;
+        for(var i = 0, len = handlers.length; i < len; i++){
+            wrapFn = handlers[i];
+            wrapFn.h.call(wrapFn.scope, topic, msg, wrapFn.data);
+        }
+    };
+
+    var deleteWrapFn = function(h, id){
+        for(var i = 0, len = h.length; i < len; i++){
+            if(h[i].sid == id){
+                h.splice(i,1);
+                break;
+            }
+        }
+    };
+
+    var match = function(p, t){
+        if(p == t){
+            return true;
+        }
+        p = p.split('.');
+        t = t.split('.');
+
+        if(t.length > p.length){
+            return false;
+        }
+
+        var flag = true;
+
+        for(var i = 0, len = p.length; i < len; i++){
+            if(t[i] == '**'){
+                break;
+            }else if(p[i] != t[i] && t[i] != '*'){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    };
+
+    var query = function(topic){
+        var msgs = [];
+        for(var p in pubItems){
+            if(match(p, topic)){
+                msgs.push({topic : p, value : pubItems[p]});
+            }
+        }
+        return msgs;
+    };
+
+    var MessageBus = {
+        version : '1.0',
+
+        subscribe : function(topic, handler, scope, data, cache){
+            checkSubTopic(topic); 
+            checkIllegalCharactor(topic);
+            scope = scope || window;
+
+            var sid = generateId();
+            var wrapFn = {h : handler, scope : scope, data : data, sid : sid};
+            var path = topic.split('.'), i = 0, len = path.length;
+            
+            (function(path, index, handler, tree){
+                var token = path[index];
+                if(index == path.length){
+                    tree.h.push(handler); 
+                }else{
+                    if(!tree.t[token]){
+                        tree.t[token] = {t:{}, h:[]};
+                    }
+                    arguments.callee.call(this, path, ++index, handler, tree.t[token]);
+                }
+            })(path, 0, wrapFn, subTree);
+            
+            if(config.cache && !!cache){
+                var msgs = query(topic);
+                for(i = 0, len = msgs.length; i < len; i++){
+                    handler.call(scope, msgs[i].topic, msgs[i].value, data);
+                }
+            }
+            return topic + '^' + sid;
+        },
+        publish : function(topic, msg){
+            checkPubTopic(topic);
+            checkIllegalCharactor(topic);
+
+            pubItems[topic] = msg;
+
+            var path = topic.split('.');
+            var tree = subTree;
+            var token;
+
+            (function(path, index, tree, msg, topic){
+                var token = path[index];
+                if(index == path.length){
+                    doCall(topic, msg, tree.h);
+                }else{
+                    if(tree.t['**']){
+                        doCall(topic, msg, tree.t['**'].h);
+                    }
+                    if(tree.t[token]){
+                        arguments.callee.call(this, path, index + 1, tree.t[token], msg, topic);
+                    }
+                    if(tree.t['*']){
+                        arguments.callee.call(this, path, index + 1, tree.t['*'], msg, topic);
+                    }
+                }
+            })(path, 0, tree, msg, topic);
+        },
+        unsubscribe : function(sid){
+            var sid = sid.split('^');
+            if(sid.length != 2){
+                throwException('illegal sid:' + sid);
+            }
+            var path = sid[0].split('.');
+            var id = sid[1];
+            
+            (function(path, index, tree, id){
+                var token = path[index];
+                if(index == path.length){
+                    deleteWrapFn(tree.h, id);
+                }else{
+                    if(tree.t[token]){
+                        arguments.callee.call(path, ++index, tree.t[token], id);
+                    }            
+                }
+            })(path, 0, subTree, id);
+        },
+        setConfig : function(c){
+            if(c && toString.call(c) == '[object Object]'){
+                for(var p in c){
+                    config[p] = c[p];
+                }
+            }
+        }
+    }; 
+
+    window.MessageBus = MessageBus;
+})(window,undefined);
--- README.md
+++ README.md
@@ -2,3 +2,55 @@ MessageBus
 ==========
 
 一个简单的消息总线
+
+## 简介 ##
+
+一个简单的消息总线——广播方式，用于组件间的通信。对基于事件绑定的通信方式进行解耦。
+支持简单的消息订阅和发布, 支持通配符订阅, **支持先发布后订阅**。
+
+## 主题格式 ##
+
+以'.'作为分隔符的字符串，支持'\*'和'\*\*'作为通配符，'\*\*'后不能再有字符；类似Java中
+包的写法，如：com.github.xwcoder。不能含有如下符号^  
+'\*'通配一个部分  
+'\*\*'通配余下的所有部分
+
+com.\*.xwcoder  com.github.\*  \*.github.xwcoder都匹配com.github.xwcoder  
+\*\*  com.\*\* 匹配com.github.xwcoder
+
+## api ##
+
+### 全局总线 ###
+window.MessageBus。所有消息的订阅和发布动作都通过window.MessageBus。总线提供4个方法,如下。
+
+### setConfig ###
+MessageBus.setConfig({cache:boolean})。目前只支持一个参数cache, true:支持后订阅模式, false:不支持后订阅模式, 默认为true。
+
+### publish ###
+MessageBus.publish(topic, msg)。在某个主题发布消息。
+
+topic : 主题。如com.github.xwcoder, 不能含有通配符。  
+msg : 发布的消息内容。可以是任何类型
+
+### subscribe ###
+MessageBus.subscribe(topic, handler, scope, data, cache)。订阅某主题。
+
+topic : 主题。如com.github.xwcoder, 可以含有通配符。
+handler : function. 当有消息在订阅上发布时的处理函数。function(topic, msg, data){//doSomething}
+            *topic : 发布消息的主题
+            *msg : 发布的消息内容
+            *data : 订阅时传递的数据
+scope : handler执行的作用域，默认是window
+data : 可以用来给handler传递数据
+cache : 订阅是否支持订阅模式, 默认是false
+
+此方法有一个返回值sid, 一个唯一的字符串标识本次订阅。用于取消订阅。
+
+### unsubscribe ###
+MessageBus.unsubscribe(sid)。取消某次订阅。
+
+sid : subscribe方法的返回值。
+
+##文件说明##
+MessageBus-v1.js、 MessageBus-v2.js ：是同一版本的不同写法。  
+demo-v1.html、demo-v2.html ：demo页面
